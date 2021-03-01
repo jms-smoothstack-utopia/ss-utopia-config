19086b003abd37796f85a39191edd9bfb86770af
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-03-01 12:48:07 -0500

    Initial commit

---
+++ src/test/java/com/ss/utopia/config/UtopiaConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.utopia.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtopiaConfigurationServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
